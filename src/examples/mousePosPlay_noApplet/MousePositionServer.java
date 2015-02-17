8a87b7db2b9a11382fbb3e9315430f9455fd9ef8
adriansi <simion.adrian@gmail.com>
2015-02-17 22:03:39 +0100

    Add the JAAS Lib Helpers
    Add the JAAS Lib Helpers

---
+++ src/examples/mousePosPlay_noApplet/MousePositionServer.java
@@ -0,0 +1,8 @@
+package examples.mousePosPlay_noApplet;
+
+interface MousePositionServer {
+    /**
+       @return x position in pixels
+    */
+    public double getMouseXPosition();
+}
---
+++ src/examples/testColorSonificator_noApplet/ColorSonificatorApplet.java
@@ -0,0 +1,25 @@
+package examples.testColorSonificator_noApplet;
+
+import jass.render.*;
+import jass.engine.*;
+import jass.generators.*;
+import java.util.*;
+import java.net.*;
+import java.io.*;
+import java.applet.*;
+
+public class ColorSonificatorApplet extends Applet {
+
+    public void init() {
+        //syfile = getParameter("syfile");
+    }
+    
+    public void start() {
+        try {
+            new TestColorSonificator(null);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
